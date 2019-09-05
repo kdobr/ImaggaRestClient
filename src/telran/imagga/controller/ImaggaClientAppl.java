c56004e01147b39c1d00b7e7cec364dee82c5f35
Konstantin Dobrynskiy <kdobr@yandex.ru>
2019-09-05 19:44:21 +0300

    first commit

--- src/telran/imagga/controller/ImaggaClientAppl.java
+++ src/telran/imagga/controller/ImaggaClientAppl.java
@@ -1,5 +1,6 @@
 package telran.imagga.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.HttpHeaders;
@@ -14,21 +15,22 @@ import telran.imagga.dto.Tag;
 
 public class ImaggaClientAppl {
 
-	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplate();
-		String url = "https://api.imagga.com/v2/tags";
-		String imgUri = "https://24smi.org/public/media/235x307/person/2017/12/22/4sqqykgn04bo-cheburashka.jpg";
-		UriComponentsBuilder builder = 
-				UriComponentsBuilder.fromHttpUrl(url)
-				.queryParam("image_url", imgUri)
-				.queryParam("limit", "3")
-				.queryParam("language", "ru,en,fr");
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "credentials");
-		RequestEntity<String> request = 
-				new RequestEntity<>(headers, HttpMethod.GET, builder.build().toUri());
-		ResponseEntity<ResultDto> response =
-				restTemplate.exchange(request, ResultDto.class);
+	public static void main(String[] args) throws IOException {
+		
+		
+		  RestTemplate restTemplate = new RestTemplate(); 
+		  String url =
+		  "https://api.imagga.com/v2/tags"; String imgUri =
+		  "https://vokzal.ru/blog/wp-content/uploads/2018/07/30-07-2018-02.jpg";
+		  UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+		  .queryParam("image_url", imgUri) .queryParam("limit", "5")
+		  .queryParam("language", "ru,en,fr"); HttpHeaders headers = new HttpHeaders();
+		  headers.add("Authorization",
+		  "Basic YWNjX2U5OWUxMzU3NzIxOWU4MToyMjJlYzE5Y2VmMDkzYjMxZjQxNGFmMTA1ODZiMWZjNQ=="
+		  ); RequestEntity<String> request = new RequestEntity<>(headers,
+		  HttpMethod.GET, builder.build().toUri()); ResponseEntity<ResultDto> response
+		  = restTemplate.exchange(request, ResultDto.class);
+		
 		ResultDto resultDto = response.getBody();
 		System.out.println(resultDto.getStatus());
 		List<Tag> tags = resultDto.getResult().getTags();
